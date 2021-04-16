9da50967f0674ece11cfbb99ece1b99bf73e0eee
Yiğit Şık <yigit6958@gmail.com>
2021-04-16 19:34:28 +0300

    Add new features and bind MongoDB to Heroku

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
-    let comp = Compress({
+var imageWidth;
+var imageHeight;
+
+
+let comp = Compress({
     inputSelector: '#input_cmprss',
     downloadSelector: '#comp_download',
     imageSelector: '#comp_img',
@@ -16,23 +20,21 @@
     console.log('listen to the compressed event to get the array');
     console.groupEnd();
 
-
     $('#output_image').attr('src',files[0]);
 
 
+        var img = new Image();
+        //Get Uploaded Image Dimensions
+            img.onload = function(){
 
-        // var img = new Image();
-        //
-        // img.onload = function(){
-        //     var height = img.height;
-        //     var width = img.width;
-        //
-        //      alert(img.height*img.width*24/1024);
-        // }
-        //
-        // img.src = files;
+                imageWidth = img.width;
+                imageHeight = img.height;
+                $('#oDimen').text(imageWidth+" X "+imageHeight);
+            }
+            img.src = files;
 
-        fetch(files[0])
+
+        fetch(files)
             .then(res => res.blob()) // Gets the response and returns it as a blob
             .then(blob => {
                 $('#Ofile_size').text((blob.size/1024).toFixed(1)+ "  kb");
@@ -42,22 +44,67 @@
     });
 
 
+
     comp.on('compressing', () =>
     {
         console.log("compressing");
+
     });
 
+    //Set Option Value:Width
+    $( "#width" ).change(function() {
+
+        var width = $("#width").val();
+        var currentHeight=  $( "#height" ).val();
+        console.log("assiging width "+width);
+        if (currentHeight==0)
+        {
+            comp.options.dimen = {width:width,height:imageHeight};
+        }
+        else
+        {
+            comp.options.dimen = {width:width,height:currentHeight};
+        }
+    });
+    //Set Option Value:Width
+
+    //Set Option Value:Height
+    $( "#height" ).change(function() {
+
+        var height = $("#height").val();
+        var currentWidth=  $( "#width" ).val();
+        console.log("assiging height "+height);
+        if (currentWidth==0)
+        {
+            comp.options.dimen = {width:imageWidth,height:height};
+        }
+        else
+            comp.options.dimen = {width:currentWidth,height:height};
+
+        console.log(comp.options.dimen);
+    });
+    //Set Option Value:Height
 
 
-    //This Function Gets the Original Image and Puts it To Input Image Card
+
+    //This Function Gets from input the Original Image and Puts it To Input Image Card
     function readURLIn(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
+            var img = new Image();
+
 
             reader.onload = function (e) {
                 $('#input_Image').attr('src', e.target.result);
                 $('#file_size').text((input.files.item(0).size/1024).toFixed(1) + "  kb");
                 $('#file_type').text(input.files.item(0).type);
+                $('#file_name').text( input.files.item(0).name);
+                $('#Ifile_name').text( input.files.item(0).name);
+
+                img.onload = (function (){
+                    $('#iDimen').text(img.width+" X "+img.height);
+                })
+                img.src = e.target.result;
             }
 
             reader.readAsDataURL(input.files[0]);
@@ -67,8 +114,10 @@
     $(".read").change(function(){
         readURLIn(this);
     });
+    //This Function Gets from input the Original Image and Puts it To Input Image Card
 
 
-
-
-
+    // function GetDim() // this works with the tag in img element ==> onload="GetDim()"
+    // {
+    // console.log( $("#comp_img").width)
+    // }
--- server.js
+++ server.js
@@ -2,6 +2,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json(),bodyParser.urlencoded({extended:true})); //This part lets you reach the html elements
+
+//Include Mongoose
+
+    const mongoose = require('mongoose');
+
+    mongoose.connect("mongodb+srv://admin-cvlt:grind_code66@cluster0.ovzyp.mongodb.net/CompLog", {useNewUrlParser: true, useUnifiedTopology: true});
+
+    const imageSchema = new mongoose.Schema({
+
+
+        fileName: String,
+        fileType: String,
+        fileSize: String,
+        OfileSize: String,
+        OfileType: String,
+        inputDimensions: String,
+        outputDimensions: String,
+        compRate: Number,
+        psnr: Number,
+        comp_ratio: Number
+
+    });
+
+    const imgData = mongoose.model("imgData", imageSchema);
+
 
 app.get("/upload.jpg",function (request,response) {
     response.sendFile(__dirname+"/upload.jpg");
@@ -17,6 +44,25 @@ app.get("/index.js",function (request,response) {
 
 app.get("/node_modules/compress-js/dist/compressjs.js",function (request,response) {
     response.sendFile(__dirname+"/node_modules/compress-js/dist/compressjs.js");
+})
+
+app.post("/",function (req,res)
+{
+
+    console.log("Post Request !");
+
+    // console.log(req.body);
+
+    const imgSample = new imgData ({
+
+        fileType: req.body.file_type,
+        fileSize: req.body.file_size,
+        inputDimensions: req.body.iDimen,
+        outputDimensions: req.body.oDimen,
+    });
+
+    imgSample.save((err, doc) => console.log(doc));
+
 })
 
 app.listen(process.env.PORT || port, function (){
